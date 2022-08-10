4277767f3e309b1da9a421e5a93d5b9f83c6eb12
Daniel Haanpaa [Lab0] <dhaanpaa@djhent.us>
2022-08-09 21:45:48 -0700

    Added checks in main script for running script

--- see/index.js
+++ see/index.js
@@ -27,12 +27,13 @@ console.log(Object.keys(scriptList));
 console.log(hostConfig)
 
 // 5... Check if script requested is in script list and not in special scripts list
-if (hostConfig.scriptToExecute in hostConfig.scripts_to_run_before) {
+
+if (hostConfig.scripts_to_run_before.includes(hostConfig.scriptToExecute)) {
     console.error("ERROR: Cannot request to run script that is in scripts_to_run_before config array")
     process.exit(1)
 }
 
-if (hostConfig.scriptToExecute in hostConfig.scripts_to_run_after) {
+if (hostConfig.scripts_to_run_after.includes(hostConfig.scriptToExecute)) {
     console.error("ERROR: Cannot request to run script that is in scripts_to_run_after config array")
     process.exit(1)
 }
@@ -41,3 +42,6 @@ if (!(hostConfig.scriptToExecute in scriptList)) {
     console.error("ERROR: Cannot request to run script that does not exist")
     process.exit(1)
 }
+
+let scriptsToBeExecuted = [...hostConfig.scripts_to_run_before, hostConfig.scriptToExecute, ...hostConfig.scripts_to_run_after]
+console.log(scriptsToBeExecuted)
